1a4f9ac31846f0d2f9fedf6e145f473f07657dbe
weibocheng <352725515@qq.com>
2018-08-21 16:46:00 +0800

    优化

--- 身份证末位计算.py
+++ 身份证末位计算.py
@@ -3,10 +3,13 @@ last_character = ['1', '0', 'x', '9', '8', '7', '6', '5', '4', '3', '2', '1']
 n, index = 0, 0
 
 while n < 17:
-    ipt = input('enter a number:')
-    index += int(ipt) * param[n]
-    n += 1
-
-index %= 11
-
-print('The last:', last_character[index])
+    ipt = input('enter a number:').strip()
+    if ipt.isdigit():
+        index += int(ipt) * param[n]
+        n += 1
+    else:
+        print("非法输入！")
+        break
+else:
+    index %= 11
+    print('The last:', last_character[index])
